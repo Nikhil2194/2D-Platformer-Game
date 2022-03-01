3e4598b5cb970b4ad162b7f2cf9955a91bb270fc
Nikhil2194 <ndahake21@gmail.com>
2022-03-01 13:05:38 +0530

    Added Key, Scripts, Player Destroys

---
+++ Assets/Scripts/ScoreController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+    public class ScoreController : MonoBehaviour
+    {
+        private TextMeshProUGUI scoreText;
+        private int score = 0;
+
+        private void Start()
+        {
+            scoreText = GetComponent<TextMeshProUGUI>();
+        }
+
+        public void ScoreUpdate(int increment)
+        {
+            score += increment;
+            RefreshGUI();
+        }
+
+        private void RefreshGUI()
+        {
+            scoreText.text = "Score : " + score;
+        }
+    }
